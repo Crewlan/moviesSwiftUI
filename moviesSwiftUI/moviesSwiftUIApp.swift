477c4f852b2a11c33c37a80ff9b89308d7f89874
Crewlan <rodrigoa.work@gmail.com>
2025-12-25 02:32:47 -0300

    Initial Commit

---
+++ moviesSwiftUI/moviesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesSwiftUIApp.swift
+//  moviesSwiftUI
+//
+//  Created by Rodrigo Silva on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct moviesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
